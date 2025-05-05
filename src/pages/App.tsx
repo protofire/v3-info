40d660b94a4ccf223206ea1eb5c54018ece635ba
msvstj <savastijonok@gmail.com>
2025-05-05 10:54:04 +0300

    Revert "chore: add warning banner"
    This reverts commit d99905e859235e0308986eb8bfe6a0944a830180.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -125,11 +125,6 @@ export default function App() {
         <AppWrapper>
           <URLWarning />
           <HeaderWrapper>
-            <WarningWrapper>
-              <WarningBanner>
-                {`Some data may be missing — we're currently working on restoring full data. Thank you for your patience.`}
-              </WarningBanner>
-            </WarningWrapper>
             {showNotSyncedWarning && (
               <WarningWrapper>
                 <WarningBanner>
