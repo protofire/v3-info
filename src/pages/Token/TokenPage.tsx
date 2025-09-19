00cb05af34cabd04743ff07822a50d711f421a61
den <dzianis.smalonski@protofire.io>
2025-09-19 11:37:24 +0200

    fix: streamline StyledExternalLink component for trade button

--- src/pages/Token/TokenPage.tsx
+++ src/pages/Token/TokenPage.tsx
@@ -241,9 +241,7 @@ export default function TokenPage() {
                         </RowBetween>
                       </ButtonGray>
                     </StyledExternalLink>
-                    <StyledExternalLink
-                      href={`https://staging.shapeswap.xyz/#/swap?inputCurrency=${formattedAddress}`}
-                    >
+                    <StyledExternalLink href={`https://staging.shapeswap.xyz/#/swap?inputCurrency=${formattedAddress}`}>
                       <ButtonPrimary width="100px" bgColor={backgroundColor} style={{ height: '44px' }}>
                         Trade
                       </ButtonPrimary>
