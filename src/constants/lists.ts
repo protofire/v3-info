6db5a2ac8fb374d50b48706930bb60b2c808a014
Leoni Mella <leoni.mella@gmail.com>
2025-12-11 17:03:49 -0300

    chore: update token list url

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -10,8 +10,7 @@ export const INK_LIST = getTokenListApiURL('ink')
 export const ABSTRACT_LIST = getTokenListApiURL('abstract')
 export const ANIME_TESTNET_LIST = getTokenListApiURL('anime-testnet')
 export const MODE_LIST = getTokenListApiURL('mode')
-// export const STABLE_LIST = 'https://assets.swap.w3us.site/networks/stable.json'
-export const STABLE_LIST = 'https://raw.githubusercontent.com/protofire/token-list/refs/heads/main/networks/stable.json'
+export const STABLE_LIST = 'https://assets.swap.w3us.site/networks/stable.json'
 
 // default lists to be 'active' aka searched across
 export const DEFAULT_ACTIVE_LIST_URLS: string[] = [
