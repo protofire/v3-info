62c030196d7938348f535fb96df5861aa3934b71
Den <dzianis.smalonski@protofire.io>
2025-12-23 13:07:09 +0100

    feat: update links to prod

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -233,7 +233,7 @@ export default function Header() {
             Tokens
           </StyledNavLink>
           <StyledExternalLink
-            href="https://staging.zircuit-swap.w3us.site"
+            href="https://zircuit-swap.w3us.site"
             target="_blank"
             rel="noopener noreferrer"
             id={`swap-nav-link`}
