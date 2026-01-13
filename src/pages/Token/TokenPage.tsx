6c029ff7a76bcc544d1551a091ba9fadbcef3739
Den <dzianis.smalonski@protofire.io>
2026-01-13 22:12:24 +0100

    fix: formatting

--- src/pages/Token/TokenPage.tsx
+++ src/pages/Token/TokenPage.tsx
@@ -241,9 +241,7 @@ export default function TokenPage() {
                         </RowBetween>
                       </ButtonGray>
                     </StyledExternalLink>
-                    <StyledExternalLink
-                      href={`staging.swap.zircuit.com/#/swap?inputCurrency=${formattedAddress}`}
-                    >
+                    <StyledExternalLink href={`staging.swap.zircuit.com/#/swap?inputCurrency=${formattedAddress}`}>
                       <ButtonPrimary width="100px" bgColor={backgroundColor} style={{ height: '44px' }}>
                         Trade
                       </ButtonPrimary>
