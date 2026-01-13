e57291516e3c338675e5c183fe23fbbec009e2c7
Den <dzianis.smalonski@protofire.io>
2026-01-13 20:19:48 +0100

    fix: remove staging

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -152,7 +152,7 @@ export default function App() {
               <BannerWrapper>
                 <UrlBanner>
                   {`info.uniswap.org is being deprecated on June 11th. Explore the new combined V2 and V3 analytics at `}
-                  <Link href={'https://staging.swap.zircuit.com/explore'}>
+                  <Link href={'https://swap.zircuit.com/explore'}>
                     <Decorator>swap.zircuit.com</Decorator>
                   </Link>
                 </UrlBanner>
--- src/pages/Token/TokenPage.tsx
+++ src/pages/Token/TokenPage.tsx
@@ -171,7 +171,7 @@ export default function TokenPage() {
             No pool has been created with this token yet. Create one
             <StyledExternalLink
               style={{ marginLeft: '4px' }}
-              href={`staging.swap.zircuit.com/#/add/${formattedAddress}`}
+              href={`swap.zircuit.com/#/add/${formattedAddress}`}
             >
               here.
             </StyledExternalLink>
@@ -233,7 +233,7 @@ export default function TokenPage() {
                 </AutoColumn>
                 {activeNetwork !== EthereumNetworkInfo ? null : (
                   <RowFixed>
-                    <StyledExternalLink href={`staging.swap.zircuit.com/#/add/${formattedAddress}`}>
+                    <StyledExternalLink href={`swap.zircuit.com/#/add/${formattedAddress}`}>
                       <ButtonGray width="170px" mr="12px" height={'100%'} style={{ height: '44px' }}>
                         <RowBetween>
                           <Download size={24} />
@@ -242,7 +242,7 @@ export default function TokenPage() {
                       </ButtonGray>
                     </StyledExternalLink>
                     <StyledExternalLink
-                      href={`staging.swap.zircuit.com/#/swap?inputCurrency=${formattedAddress}`}
+                      href={`swap.zircuit.com/#/swap?inputCurrency=${formattedAddress}`}
                     >
                       <ButtonPrimary width="100px" bgColor={backgroundColor} style={{ height: '44px' }}>
                         Trade
